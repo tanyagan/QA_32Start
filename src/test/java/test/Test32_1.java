2145a72ff6b412ba28c99bcfc3bba595426616c8
tanyagan <avgustan2012@gmail.com>
2022-03-16 16:29:21 +0200

    first webinar

---
+++ src/test/java/test/Test32_1.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Test32_1 {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preConditions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        wd = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Tatiana/Automatization/Qa_32Start/chromedriver.exe");
+
+    }
+    @Test
+
+    @AfterMethod
+}
